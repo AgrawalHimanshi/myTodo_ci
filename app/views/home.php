869777d378283e3105795e84965b0016bd27ef0f
Himanshi Agrawal <42604981+AgrawalHimanshi@users.noreply.github.com>
2019-04-16 21:37:55 +0530

    Add files via upload

---
+++ app/views/home.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>myTodo </title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+</head>
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<a class="brand" href="#">myTodo</a>
+				<div class="nav-collapse collapse">
+					<p class="navbar-text pull-right">
+						<!--RIGHT TOP CONTENT-->
+					</p>
+					<ul class="nav">
+						<li><a href="#">Home</a></li>
+					</ul>
+				</div><!--/.nav-collapse-->
+			</div>
+		</div>
+	</div>
+
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span3">
+				<div class="well sidebar-nav">
+					<div style="margin: 0 0 10px 10px;">
+						<!--SIDEBAR CONTENT-->
+					</div>
+				</div><!--/.WELL-->
+			</div><!--/SPAN-->
+			<div class="span9">
+				<!--main content-->
+				<h1>Welcome to myTodo!</h1>
+				<p>myTodo is a simple and helpful application to help you manage yoour day to day tasks. You can add as many task lists as you like. myTodo is absolutely free Have fun!!</p>
+			</div><!--/span-->
+		</div><!--row-->
+		<hr>
+		<footer>
+			<p>&copy; Copyright 2019</p>
+		</footer>
+	</div><!--/.fluid-container-->
+</body>
+</html>
